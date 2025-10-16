346a9d04bc42f07bfc08748eb14cb875cc1faf2c
luckyhut <ed7johnson@gmail.com>
2025-10-15 21:30:36 -0600

    Fixing intentionally broken code

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORT fail test")
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
